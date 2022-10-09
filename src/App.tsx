370a95de4bb23214893e184c1c1ebf141f6b22bd
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2022-10-09 18:58:30 +0200

    chore: remove navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,33 @@ import { Download } from "./components/Download";
 import { Editor } from "./components/Editor";
 import { SaveUpload } from "./components/SaveUpload";
 import { useSaveStore } from "./stores/saveStore";
-import Navbar from "./components/UI/Layout/Navbar";
 
 function App() {
   const { isLoaded } = useSaveStore();
 
   return (
     <>
-      <Navbar />
       <div className="flex flex-col items-center w-full h-screen justify-center">
-
-        {isLoaded && <div className="justify-center font-medium text-2xl drg-framer text-center min-w-[400px] max-w-[90%] w-auto py-2 mb-6 uppercase">DRG Editor</div>}
+        {isLoaded && (
+          <div className="justify-center font-medium text-2xl drg-framer text-center min-w-[400px] max-w-[90%] w-auto py-2 mb-6 uppercase">
+            DRG Editor
+          </div>
+        )}
 
         <div className=" shadow-md min-w-[400px] max-w-[90%] p-6 bg-gray-800 drg-framer !border-b-[15px] max-h-full transition-all duration-1000">
           {!isLoaded && (
             <>
               <div className="flex items-center justify-center">
-                <img src='assets/logo.png' alt="logo" className="w-32 mx-auto" />
-                <img src='assets/season02.png' alt="season logo" className="w-32 mx-auto" />
+                <img
+                  src="assets/logo.png"
+                  alt="logo"
+                  className="w-32 mx-auto"
+                />
+                <img
+                  src="assets/season02.png"
+                  alt="season logo"
+                  className="w-32 mx-auto"
+                />
               </div>
               <SaveUpload />
             </>
@@ -27,7 +36,8 @@ function App() {
           {isLoaded && <Editor />}
           {isLoaded && <Download />}
         </div>
-      </div></>
+      </div>
+    </>
   );
 }
 
