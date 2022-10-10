8092a6ea6db055a03552500cb532758e75f25935
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2022-10-11 00:44:30 +0200

    chore: configure vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), ViteWebfontDownload()],
-  base: "/drg-editor/",
+export default defineConfig(({ mode }) => {
+  if (mode === "vercel") {
+    return {
+      plugins: [react(), ViteWebfontDownload()],
+    };
+  }
+  return {
+    plugins: [react(), ViteWebfontDownload()],
+    base: "/drg-editor/",
+  };
 });
