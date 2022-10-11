9e9864c72d7120c69b95b65b368fbe730b4423bb
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2022-10-11 12:57:26 +0200

    test: add image regresion testing

---
+++ src/__tests__/snapshots/updateSnapshots.spec.ts
@@ -0,0 +1,70 @@
+import { toMatchImageSnapshot } from "jest-image-snapshot";
+import type { Browser, ElementHandle, Page } from "puppeteer";
+import { launch } from "puppeteer";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+expect.extend({ toMatchImageSnapshot });
+
+describe("update snapshots", () => {
+  let browser: Browser;
+  let page: Page;
+
+  const waitForTransition = async () => new Promise((r) => setTimeout(r, 300));
+
+  beforeAll(async () => {
+    browser = await launch();
+    page = await browser.newPage();
+    page.setViewport({ width: 1430, height: 1180 });
+
+    await page.goto("http://localhost:5173/drg-editor/");
+    await page.waitForNetworkIdle();
+  });
+
+  it("Update homepage snapshot", async () => {
+    const image = await page.screenshot();
+
+    await waitForTransition();
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it("Update resources snapshot", async () => {
+    const fileInput = (await page.$(
+      "input[type=file]"
+    )) as ElementHandle<HTMLInputElement>;
+    if (!fileInput) {
+      throw new Error("File input not found");
+    }
+
+    console.log("Uploading file...");
+    await fileInput.uploadFile("src/__tests__/fixtures/example.sav");
+    await page.waitForSelector("ul.menu");
+
+    await waitForTransition();
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it("Update season snapshot", async () => {
+    await page.click("li:nth-child(2)");
+    await page.click(".block:nth-child(1) button");
+
+    await waitForTransition();
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  it("Update dwarf snapshot", async () => {
+    await page.click("li:nth-child(3)");
+
+    await waitForTransition();
+    const image = await page.screenshot();
+
+    expect(image).toMatchImageSnapshot();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+});
