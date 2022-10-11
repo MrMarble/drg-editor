0debed7e70444bd64237c89aeffb2bba59944ce3
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2022-10-11 12:57:54 +0200

    chore: add script to update snapshots

---
+++ tools/update_snapshots.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp src/__tests__/snapshots/__image_snapshots__/*homepage*.png assets/first.png
+cp src/__tests__/snapshots/__image_snapshots__/*resources*.png assets/second.png
+cp src/__tests__/snapshots/__image_snapshots__/*season*.png assets/third.png
+cp src/__tests__/snapshots/__image_snapshots__/*dwarf*.png assets/forth.png
