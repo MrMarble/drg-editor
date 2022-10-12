2e75e5ed691c3adf278d4397f245ba0e42ffe3e8
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2022-10-12 10:12:49 +0200

    test(u8array): improve tests

--- src/helpers/u8array/__spec__/uin8array.spec.ts
+++ src/helpers/u8array/__spec__/uin8array.spec.ts
@@ -1,24 +1,43 @@
 import { readFileSync } from "fs";
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
+import { DWARFS, UUIDS } from "../../../constant";
+import { hexStringToByteArray } from "../../hexToByte";
 import { U8Array } from "../uint8array";
 
 describe("U8Array", () => {
   const buff = readFileSync("src/__tests__/fixtures/example.sav");
+  let haystack: U8Array;
 
-  test("Should find index array", () => {
-    const haystack = new U8Array(buff);
-    const needle = [
-      0x07, 0x85, 0x48, 0xb9, 0x32, 0x32, 0xc0, 0x40, 0x85, 0xf8, 0x92, 0xe0,
-      0x84, 0xa7, 0x41, 0x00,
-    ];
-
-    expect(haystack.indexOfMulti(needle)).toBe(197355);
+  beforeEach(() => {
+    haystack = new U8Array([...buff]);
   });
 
-  test("Should find index string", () => {
-    const haystack = new U8Array(buff);
-    const needle = "078548b93232c04085f892e084a74100";
+  const cases = [
+    { uuid: UUIDS[DWARFS.DRILLER] + "030000005850", type: "string" },
+    {
+      uuid: hexStringToByteArray(UUIDS[DWARFS.DRILLER] + "030000005850"),
+      type: "bytes",
+    },
+  ];
 
-    expect(haystack.indexOfMulti(needle)).toBe(197355);
+  describe.each(cases)("UUID as $type", ({ uuid }) => {
+    test(`Should find the UUID ${uuid}`, () => {
+      expect(haystack.indexOfMulti(uuid)).toBe(43723);
+    });
+
+    test("Should be able to get int32", () => {
+      const offset = 26; // xp offset
+      const value = 6093;
+      expect(haystack.getInt32(uuid, offset)).toBe(value);
+    });
+
+    test("Should be able to set int32", () => {
+      const offset = 26; // xp offset
+      const value = 1337;
+
+      haystack.setInt32(uuid, offset, value);
+
+      expect(haystack.getInt32(uuid, offset)).toBe(value);
+    });
   });
 });
