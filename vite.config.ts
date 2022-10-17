ede21a1192262bf8415844ebd12cd01e319c1bfb
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2022-10-17 12:29:52 +0200

    chore: support gzip

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,31 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import Compression from "vite-compression-plugin";
 import checker from "vite-plugin-checker";
 import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  const config = {
+    plugins: [
+      react(),
+      ViteWebfontDownload(),
+      checker({
+        typescript: true,
+        eslint: {
+          lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+        },
+      }),
+      Compression(),
+    ],
+  };
+
   if (mode === "vercel") {
-    return {
-      plugins: [
-        react(),
-        ViteWebfontDownload(),
-        checker({
-          typescript: true,
-          eslint: {
-            lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
-          },
-        }),
-      ],
-    };
+    return config;
   }
+
   return {
-    plugins: [react(), ViteWebfontDownload()],
+    ...config,
     base: "/drg-editor/",
   };
 });
