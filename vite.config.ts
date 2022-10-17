dc7815d6651864563dcb0f947e9457d2ff7d0ab6
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2022-10-17 12:00:10 +0200

    chore: lint with vite server

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   if (mode === "vercel") {
     return {
-      plugins: [react(), ViteWebfontDownload()],
+      plugins: [
+        react(),
+        ViteWebfontDownload(),
+        checker({
+          typescript: true,
+          eslint: {
+            lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+          },
+        }),
+      ],
     };
   }
   return {
